9d794ba3a15657c83843b25eca4d8313a35b4e23
Shane Harrell <shane.harrell@signalwire.com>
2023-01-13 14:23:17 -0500

    blah

--- run.py
+++ run.py
@@ -9,9 +9,9 @@ robokiller = Flask(__name__)
 @robokiller.route('/')
 def go_robokiller():
     _api_key = os.environ.get('RK_API_KEY', None)
-    _to = request.args.get('To')
-    _from = request.args.get('From')
-    _dest = request.args.get('Dest')
+    _to = quote(request.args.get('To'))
+    _from = quote(request.args.get('From'))
+    _dest = quote(request.args.get('Dest'))
     if all(v is not None for v in [_api_key, _to, _from, _dest]):
         _data = json.loads(requests.get('https://enterprise-api.robokiller.com/v1/reputation?from=' + _from + '&to=' + _to + '&api_key=' + _api_key).content)
 
